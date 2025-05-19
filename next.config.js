3a8079b92a95fba3ca0c1775993ceb102fb0a8fa
Jonathan Dominguez <proyectointegradorjnt@gmail.com>
2025-05-19 09:06:32 +0000

    no generaste el codigo

---
+++ next.config.js
@@ -0,0 +1,30 @@
+
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'aqcmdxuxkolbzmfljcxx.supabase.co', // Asegúrate que este sea tu ID de proyecto Supabase
+        port: '',
+        pathname: '/storage/v1/object/public/**',
+      },
+    ],
+  },
+};
+
+export default nextConfig;
